1f00925d19b4685f83cc229bea75bc046c823b74
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2021-05-25 10:32:44 +0530

    Add "MainController" and add "index" method

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Inertia\Inertia;
+use Inertia\Response;
+
+class MainController extends Controller
+{
+    public function index(): Response
+    {
+        $companies = [
+            [
+                'id' => 1,
+                'name' => 'Aye'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Bee'
+            ],
+        ];
+        return Inertia::render('Dashboard/Main', [
+            'companies' => $companies
+        ]);
+    }
+}
