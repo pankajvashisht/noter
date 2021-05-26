078c9b6bf11ba0907c4468a8e00795bdc3e7a555
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2021-05-26 20:28:26 +0530

    Use raw-query format (instead of query-builder)
    Benefits:
     - The project is supposed to be an "easy-and-fast" dashboard to look at things from a data-reporting POV, so optimising the code to follow the framework isn't necessarily required
     - The SQL can be written and optimised separately and can be moved to code to show highly tailored data
     - PhpStorm is setup in a way that allows direct query execution from within the code itself (despite using the "secondary" DB) for quick testing and frequent changes
     - Since the IDE is connecting SQL within code, so SQL-auto-complete features are also available (making the development even faster and easier - especially considering the fact that this is "meant" to be a data-reporting dashboard)
    
    In previous couple of commits, the Query-Builder format of writing the query using Laravel has been pushed, so that it can be used if/when needed. As of now, removing the Laravel code, replacing with RAW-SQL format.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -28,24 +28,15 @@ class MainController extends Controller
 
     public function companies(): Response
     {
-//        $companies = $this->db2
-//            ->select("
-//                SELECT c.id, c.name, COUNT(o.id) as ocount
-//                FROM companies c
-//                    LEFT JOIN kpi_opportunities as o ON c.id = o.company_id
-//                GROUP BY c.id, c.name
-//                HAVING COUNT(c.id) > 1
-//                ORDER BY id
-//                    ");
-//        $companies = ($this->db2)->table('companies')
-//            ->from('companies')
-//            ->selectRaw('companies.id, companies.name, COUNT(kpi_opportunities.id) as ocount')
-//            ->leftJoin('kpi_opportunities', 'companies.id', '=', 'kpi_opportunities.company_id')
-//            ->groupBy('companies.id', 'companies.name')
-//            ->havingRaw('COUNT(companies.id) > 1')
-//            ->orderBy('companies.id')
-//            ->get()
-//            ->toArray();
+        $companies = $this->db2
+            ->select("
+                SELECT c.id, c.name, COUNT(o.id) as ocount
+                FROM companies c
+                    LEFT JOIN kpi_opportunities as o ON c.id = o.company_id
+                GROUP BY c.id, c.name
+                HAVING COUNT(c.id) > 1
+                ORDER BY id
+                    ");
         return Inertia::render('Dashboard/Companies', [
             'companies' => $companies
         ]);
