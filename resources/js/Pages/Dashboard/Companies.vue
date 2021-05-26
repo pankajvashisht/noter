57b02ddc8e4183ca14b6da94568c4c0de5db9afb
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2021-05-26 19:10:08 +0530

    Replace complex listing template with simple list

--- resources/js/Pages/Dashboard/Companies.vue
+++ resources/js/Pages/Dashboard/Companies.vue
@@ -12,62 +12,12 @@
                     <div class="p-6 bg-white border-b border-gray-200">
                         <div class="hidden mt-8 sm:block">
                             <div class="align-middle inline-block min-w-full border-b border-gray-200">
-                                <table class="min-w-full">
-                                    <thead>
-                                    <tr class="border-t border-gray-200">
-                                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            <span class="lg:pl-2">company</span>
-                                        </th>
-                                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            --
-                                        </th>
-                                        <th class="md:table-cell px-6 py-3 border-b border-gray-200 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            --
-                                        </th>
-                                    </tr>
-                                    </thead>
-                                    <tbody class="bg-white divide-y divide-gray-100">
-                                    <tr v-for="company in companies" :key="company.id">
-                                        <td class="px-6 py-3 w-2/12 whitespace-nowrap text-sm font-medium text-gray-900">
-                                            <div class="flex items-center space-x-3 lg:pl-2">
-                                                <div :class="['flex-shrink-0 w-2.5 h-2.5 rounded-full']" aria-hidden="true" />
-                                                <a href="#" class="truncate hover:text-gray-600">
-                                                <span>
-                                                  {{ company.name }}
-                                                </span>
-                                                </a>
-                                            </div>
-                                        </td>
-                                        <td class="px-6 py-3 text-sm text-gray-500 font-medium w-3/12">
-                                            <ul class="mt-3">
-                                                <li class="col-span-2 flex shadow-sm rounded-md">
-                                                    <div class="w-2/12 flex-shrink-0 flex items-center justify-center bg-green-500 text-white text-sm font-medium rounded-l-md">
-                                                        Opps
-                                                    </div>
-                                                    <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                                        <div class="flex-1 px-4 py-2 text-sm truncate">
-                                                            <p class="text-gray-500"> {{ company.ocount }}</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <li class="col-span-2 flex shadow-sm rounded-md mt-2">
-                                                    <div class="w-2/12 flex-shrink-0 flex items-center justify-center bg-blue-400 text-white text-sm font-medium rounded-l-md">
-                                                        Stages
-                                                    </div>
-                                                    <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                                        <div class="flex-1 px-4 py-2 text-sm truncate">
-                                                            <p class="text-gray-500"> {{ company.scount }}</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </td>
-                                        <td class="md:table-cell px-6 py-3 whitespace-nowrap text-sm text-gray-500 text-right w-2/12">
-                                            {{ company.id }}
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                                <ul class="space-y-3">
+                                    <li v-for="company in companies" :key="company.id" class="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md">
+                                        <span>{{ company.name }}</span>
+
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
