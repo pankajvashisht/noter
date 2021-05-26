6544deb6c888fe11c685b17fb21e2a42c9e263f3
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2021-05-26 19:06:51 +0530

    Use Query builder, instead of Raw query - losing the benefit of SQL features(in PhpStorm)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Database\Query\Builder;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -27,18 +28,24 @@ class MainController extends Controller
 
     public function companies(): Response
     {
-        $companies = $this->db2
-            ->select("
-                SELECT c.id, c.name, COUNT(o.id) as ocount
-                FROM companies c
-                    LEFT JOIN kpi_opportunities as o ON c.id = o.company_id
-                GROUP BY c.id, c.name
-                UNION ALL
-                SELECT c.id, c.name, COUNT(s.id) as scount
-                FROM companies c
-                    LEFT JOIN salesforce_opportunity_stages as s ON c.id = s.company_id
-                GROUP BY c.id, c.name
-                    ");
+//        $companies = $this->db2
+//            ->select("
+//                SELECT c.id, c.name, COUNT(o.id) as ocount
+//                FROM companies c
+//                    LEFT JOIN kpi_opportunities as o ON c.id = o.company_id
+//                GROUP BY c.id, c.name
+//                HAVING COUNT(c.id) > 1
+//                ORDER BY id
+//                    ");
+        $companies = ($this->db2)->table('companies')
+            ->from('companies')
+            ->selectRaw('companies.id, companies.name, COUNT(kpi_opportunities.id) as ocount')
+            ->leftJoin('kpi_opportunities', 'companies.id', '=', 'kpi_opportunities.company_id')
+            ->groupBy('companies.id', 'companies.name')
+            ->havingRaw('COUNT(companies.id) > 1')
+            ->orderBy('companies.id')
+            ->get()
+            ->toArray();
         return Inertia::render('Dashboard/Companies', [
             'companies' => $companies
         ]);
